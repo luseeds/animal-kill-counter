b10512349cf9b692764944ea52526b0cf4391a53
cyrilf <cyrilf7x@gmail.com>
2020-03-22 15:15:37 +0100

    Add a text section with information on the counters

--- components/Source.vue
+++ components/Source.vue
@@ -1,15 +1,22 @@
 <template>
   <section class="text-center text-gray-800 bg-gray-300 py-8">
-    <div class="text-xl uppercase mt-4 mb-4">Sources</div>
+    <div class="text-lg uppercase mt-4 mb-2">Sources</div>
     <p>
-      <a href="http://faostat3.fao.org/download/Q/QL/E">FAOSTAT 2013</a>
+      Number of land animal killed (2013)
+      <a href="http://faostat3.fao.org/download/Q/QL/E">2013</a>
     </p>
     <p>
+      Fish count estimates
       <a href="http://fishcount.org.uk/fish-count-estimates"
-        >Fish count estimates (fishcount.org.uk)</a
+        >fishcount.org.uk</a
       >
     </p>
-    <div class="text-xl uppercase mt-4 mb-4">Credits</div>
+    <p>
+      Inspired by
+      <a href="http://considerveganism.com/counter/">considerveganism</a>
+      counters
+    </p>
+    <div class="text-lg uppercase mt-4 mb-2">Credits</div>
     <p>
       Images are from
       <a href="https://unsplash.com">Unsplash</a>
--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,42 @@
       <nuxt-link to="/" class="inline-block">
         <h1 class="title">Animal Kill Counter</h1>
       </nuxt-link>
-      <h2 class="sub-title">
+      <h2 class="subtitle">
         Witness the numbers of animal killed every seconds
       </h2>
+      <section
+        class="text-gray-800 mt-8 p-4 max-w-4xl mx-auto bg-gray-300 border-l-4 border-gray-600 rounded"
+      >
+        <div>
+          Every seconds of every day, animals are being exploited, abused and
+          killed only to please our taste buds, make some of our clothes,
+          entertain ourselves or for testing purposes.
+        </div>
+        <div>
+          There is no moral justification behind that, especially in our time
+          and days where alternatives are widely available.
+        </div>
+        <br />
+        <div>
+          These numbers are so high that it's almost impossible to get a sense
+          of them.
+        </div>
+        <div>
+          They do not include the millions of animals killed in laboratories,
+          fur farms, animal shelters, zoos, marine parks, or circuses. Nor do
+          they include the animals killed by human negligence, blood sports,
+          abuse, or extermination attempts.
+        </div>
+        <br />
+        <div>
+          We should always remember that we are talking about unique sentient
+          living beings and that every life matters.
+        </div>
+        <div>
+          Below you can witness the number of animals being: Killed. Every.
+          Seconds.
+        </div>
+      </section>
       <animal-cards />
     </div>
   </div>
@@ -43,7 +76,7 @@ export default {
   @apply text-4xl font-medium text-gray-800;
 }
 
-.sub-title {
+.subtitle {
   @apply font-medium text-gray-600;
 }
 </style>
